0cfb9b347566ac78094829d12004273f01ca2413
Blake Ziegler <zieglerblake2@gmail.com>
2025-08-04 12:42:52 -0400

    clean

--- nltk_install.py
+++
@@ -1,9 +0,0 @@
-import nltk
-
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('wordnet')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('maxent_treebank_pos_tagger')
-nltk.download('maxent_ne_chunker_tab')
-nltk.download('maxent_ne_chunker_tab')
