1406df7587bb5971e2c80c439dbb688d0738b560
Blake Ziegler <zieglerblake2@gmail.com>
2025-08-04 12:48:12 -0400

    tweak

--- model/DAPT/v3/model_merge.py
+++ model/DAPT/v3/model_merge.py
@@ -20,7 +20,7 @@ model = PeftModel.from_pretrained(model, adapter_path)
 model = model.merge_and_unload()
 
 # Save final merged model in FP16
-output_path = "phinance_3.5_mini_dapt-600k_v3"
+output_path = "phinance_3.5_mini_dapt-700k_v3"
 model.save_pretrained(output_path, safe_serialization=True)
 tokenizer = AutoTokenizer.from_pretrained(base_model, trust_remote_code=True)
 tokenizer.save_pretrained(output_path)
