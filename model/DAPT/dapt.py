cedf67752d214bcf7cd2cd812b97cc994c7073c5
Blake Ziegler <zieglerblake2@gmail.com>
2025-08-06 16:48:14 -0400

    config

--- model/DAPT/dapt.py
+++ model/DAPT/dapt.py
@@ -85,8 +85,8 @@ plt.show()
 # Training Arguments
 training_args = TrainingArguments(
     output_dir=output_dir,
-    per_device_train_batch_size=2,
-    gradient_accumulation_steps=8,
+    per_device_train_batch_size=1,
+    gradient_accumulation_steps=16,
     num_train_epochs=2,
     learning_rate=2e-5,
     fp16=True,
