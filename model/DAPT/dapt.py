28159a041f24e00bd75d9865f21d737acb3bf0e5
Blake Ziegler <zieglerblake2@gmail.com>
2025-08-06 19:27:50 -0400

    fix

--- model/DAPT/dapt.py
+++ model/DAPT/dapt.py
@@ -30,6 +30,7 @@ lora_config = LoraConfig(
     lora_dropout=0.1,
     bias="none",
     task_type="CAUSAL_LM",
+    target_modules=["q_proj", "v_proj"], 
 )
 model = get_peft_model(model, lora_config)
 model.gradient_checkpointing_enable()
